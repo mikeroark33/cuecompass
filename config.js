0d02926614c23a8c8126e4fa9c6c1376145cc51a
mikeroark33 <stellarroark@gmail.com>
2026-09-22 00:45:10 -0400

    Add MAPBOX_TOKEN to config.js

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 export const SUPABASE_URL = "https://tsadezsgomoacjmxuato.supabase.co";
 export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_8JJ4_FFZnJ6GmkcEwm74ng_Tl9dPtA-";
+export const MAPBOX_TOKEN = "pk.eyJ1IjoibXVzdGFuZzMzIiwiYSI6ImNtdWM2ZXE2dzA3dTQzNHByYTh5ZDB4aXIifQ.6USr5amlmsXn1EZWAf7d0Q";
