d2ecee1c2c76e39b601635d0d9a9d9c34343bdfa
mikeroark33 <stellarroark@gmail.com>
2026-09-22 01:16:34 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-import { SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, MAPBOX_TOKEN } from "./config.js";
+import { SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY } from "./config.js";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
 let session = null;
